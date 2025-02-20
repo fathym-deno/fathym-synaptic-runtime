62449c9e8eb4cef3bd4060aa24572675f6dbc1f5
mcgear <michael.gearhardt@gmail.com>
2025-02-20 02:50:07 -0700

    ref updates and adjust

--- src/plugins/RuntimePlugin.ts
+++ src/plugins/RuntimePlugin.ts
@@ -8,8 +8,8 @@ import {
   FathymSynapticPlugin,
 } from '@fathym/synaptic';
 import { IoCContainer } from '@fathym/ioc';
-import { EaCRuntimePlugin, EaCRuntimePluginConfig } from '@fathym/eac/runtime/plugins';
-import { EaCRuntimeConfig } from '@fathym/eac/runtime/config';
+import { EaCRuntimeConfig, EaCRuntimePluginConfig } from '@fathym/eac/runtime/config';
+import { EaCRuntimePlugin } from '@fathym/eac/runtime/plugins';
 import { EaCLocalDistributedFileSystemDetails } from '@fathym/eac/dfs';
 import { EverythingAsCode } from '@fathym/eac';
 import { EaCDenoKVDetails, EverythingAsCodeDenoKV } from '@fathym/eac-deno-kv';
